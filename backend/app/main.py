3e3bdd585b8b8608b28f44ace0ff1bbdc3f1965b
tu.tran@indiez.net <tu.tran@indiez.net>
2022-02-10 16:23:19 +0700

    start

---
+++ backend/app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.core.config import settings
+from .database import db
+
+
+def get_application():
+    _app = FastAPI(title=settings.PROJECT_NAME)
+
+    _app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    db.init_app(_app)
+
+    return _app
+
+
+app = get_application()
